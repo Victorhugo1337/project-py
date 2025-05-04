ae121d1209a4a60eae7fbd7c5b0c1045c12c815c
Victorhugo1337 <hugo.almeida.vh@gmail.com>
2025-05-04 16:52:46 -0300

    fim da analise de dados

--- explorar_dados.py
+++ explorar_dados.py
@@ -1,9 +1,36 @@
 import pandas as pd
+import numpy as np
+import plotly.express as px
+import plotly.io as pio
+import streamlit as st
+pio.renderers.default = 'browser'
 
-df = pd.read_csv("dados/students_habits_perfomance")
-
-print("Prévia do dataset:")
+# Transformação em dataframe e tratamendo dos dados
+df = pd.read_csv("dados/student_habits_performance.csv")
 print(df.head())
-
-print("\nInformação do dataset:")
 print(df.info())
+print(df.describe())
+print(df.shape)
+df_clean = df.dropna()
+print(df_clean.shape)
+
+# Criando correlação entre variáveis.
+df_numeric = df_clean.select_dtypes(include=[np.number])
+correlation_matrix = df_numeric.corr()
+exam_correlation = correlation_matrix['exam_score'].sort_values(
+    ascending=False)
+print(exam_correlation)
+
+# Gráfico de correlações
+
+variables = ['study_hours_per_day', 'mental_health_rating',
+             'exercise_frequency', 'sleep_hours', 'netflix_hours', 'social_media_hours']
+
+for var in variables:
+    fig = px.scatter(df_clean, x=var, y='exam_score', title=f'Relação entre {var} e exam_score',
+                     labels={var: var, 'exam_score': 'Pontuação do Exame'},
+                     color='exam_score', color_continuous_scale='Viridis')
+    fig.show()
+
+    # Salvando o data frame clean
+    df_clean.to_csv('dados/df_clean.csv', index=False)
--- main.py
+++ main.py
@@ -1,3 +1,19 @@
 import pandas as pd
+import streamlit as st
+import plotly.express as px
 
-print(pd.__version__)
+# Dataset clean
+df_clean = pd.read_csv('dados/df_clean.csv')
+
+
+variables = ['study_hours_per_day', 'mental_health_rating',
+             'exercise_frequency', 'sleep_hours', 'netflix_hours', 'social_media_hours']
+
+# Definir título do dashboard
+st.title("Dashboard de Análise das Variáveis em Relação ao Exame")
+
+for var in variables:
+    fig = px.scatter(df_clean, x=var, y='exam_score', title=f'Relação entre {var} e exam_score',
+                     labels={var: var, 'exam_score': 'Pontuação do Exame'},
+                     color='exam_score', color_continuous_scale='Viridis')
+    st.plotly_chart(fig)
